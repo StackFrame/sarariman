7decc57b86eb5d2f91c53105ceaf6a4f0242eb32
mcculley <mcculley@75b52fa5-49ef-4cdb-a99e-094fd703196f>
2009-03-14 20:28:10 +0000

    Add version footer.

---
+++ src/java/com/stackframe/sarariman/SararimanContextListener.java
@@ -0,0 +1,37 @@
+package com.stackframe.sarariman;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ *
+ * @author mcculley
+ */
+public class SararimanContextListener implements ServletContextListener {
+
+    private final static String revision = "$Revision$";
+
+    private static String getRevision() {
+        StringBuilder buf = new StringBuilder();
+        for (int i = 0; i < revision.length(); i++) {
+            char c = revision.charAt(i);
+            if (Character.isDigit(c)) {
+                buf.append(c);
+            }
+        }
+
+        return buf.toString();
+    }
+
+    public static String version() {
+        return "1.0." + getRevision();
+    }
+
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
+
+    public void contextInitialized(ServletContextEvent sce) {
+        sce.getServletContext().setAttribute("sararimanVersion", version());
+    }
+
+}
