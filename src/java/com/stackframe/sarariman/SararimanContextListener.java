283653eae505046aa1427511c1379515067c886e
mcculley <mcculley@75b52fa5-49ef-4cdb-a99e-094fd703196f>
2009-03-14 20:28:46 +0000

    Change format of version string.

--- src/java/com/stackframe/sarariman/SararimanContextListener.java
+++ src/java/com/stackframe/sarariman/SararimanContextListener.java
@@ -24,7 +24,7 @@ public class SararimanContextListener implements ServletContextListener {
     }
 
     public static String version() {
-        return "1.0." + getRevision();
+        return "1.0r" + getRevision();
     }
 
     public void contextDestroyed(ServletContextEvent sce) {
