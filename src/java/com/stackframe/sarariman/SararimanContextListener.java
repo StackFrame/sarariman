38b7f25758848cf0e38861ee3c7571c33c07f3dd
mcculley <mcculley@75b52fa5-49ef-4cdb-a99e-094fd703196f>
2009-05-25 13:59:51 +0000

    Bump version.

--- src/java/com/stackframe/sarariman/SararimanContextListener.java
+++ src/java/com/stackframe/sarariman/SararimanContextListener.java
@@ -24,7 +24,7 @@ public class SararimanContextListener implements ServletContextListener {
     }
 
     public static String version() {
-        return "1.0.6r" + getRevision();
+        return "1.0.7r" + getRevision();
     }
 
     public void contextDestroyed(ServletContextEvent sce) {
