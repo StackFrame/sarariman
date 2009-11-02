5a8da4c160a8e8b3b971143e41d05a88ac6bc10d
mcculley <mcculley@75b52fa5-49ef-4cdb-a99e-094fd703196f>
2009-11-02 15:36:46 +0000

    Bump version.

--- src/java/com/stackframe/sarariman/Sarariman.java
+++ src/java/com/stackframe/sarariman/Sarariman.java
@@ -56,7 +56,7 @@ public class Sarariman implements ServletContextListener {
     }
 
     public String getVersion() {
-        return "1.0.15r" + getRevision();
+        return "1.0.16r" + getRevision();
     }
 
     public Sarariman() {
