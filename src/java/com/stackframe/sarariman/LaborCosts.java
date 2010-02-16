f7d9bd3597d6d02c37bd32cfb71bde7570185a1b
mcculley <mcculley@75b52fa5-49ef-4cdb-a99e-094fd703196f>
2010-02-16 13:54:28 +0000

    Deprecate the old mechanism for reusing a Connection.

--- src/java/com/stackframe/sarariman/LaborCosts.java
+++ src/java/com/stackframe/sarariman/LaborCosts.java
@@ -42,7 +42,7 @@ public class LaborCosts extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/csv;charset=UTF-8");
         String invoice = request.getParameter("id");
-        Connection connection = sarariman.getConnection();
+        Connection connection = sarariman.openConnection();
         PrintWriter out = response.getWriter();
         try {
             out.println("Employee,Task,Labor Category,Date,Rate,Duration,Cost");
@@ -79,6 +79,11 @@ public class LaborCosts extends HttpServlet {
             throw new IOException(se);
         } finally {
             out.close();
+            try {
+                connection.close();
+            } catch (SQLException se) {
+                throw new ServletException(se);
+            }
         }
     }
 
