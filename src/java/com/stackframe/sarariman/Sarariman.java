3e9204e134363714c01f0a915f8d66d243c5c6e8
mcculley <mcculley@75b52fa5-49ef-4cdb-a99e-094fd703196f>
2010-04-05 19:51:51 +0000

    Bump version.

--- src/java/com/stackframe/sarariman/Sarariman.java
+++ src/java/com/stackframe/sarariman/Sarariman.java
@@ -54,7 +54,7 @@ public class Sarariman implements ServletContextListener {
     }
 
     public String getVersion() {
-        return "1.1.8r" + getRevision();
+        return "1.1.9r" + getRevision();
     }
 
     private static Properties lookupDirectoryProperties(Context envContext) throws NamingException {
