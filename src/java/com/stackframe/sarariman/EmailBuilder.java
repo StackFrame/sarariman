da445253722c2b2b0f843ea453c41f7f8a6fd02e
mcculley <mcculley@75b52fa5-49ef-4cdb-a99e-094fd703196f>
2011-02-01 23:39:06 +0000

    Add a FIXME.

--- src/java/com/stackframe/sarariman/EmailBuilder.java
+++ src/java/com/stackframe/sarariman/EmailBuilder.java
@@ -132,6 +132,7 @@ public class EmailBuilder extends HttpServlet {
 
         sarariman.getEmailDispatcher().send(from, to, cc, subject, body, attachments, postSendAction);
 
+        // FIXME: This should redirect to a page that says this stuff.
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         try {
