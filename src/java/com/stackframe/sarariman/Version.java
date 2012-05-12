8375ec2913ff4b381ba7b5e5ec5a4cd96b7adaec
Gene McCulley <mcculley@stackframe.com>
2012-05-12 07:57:22 -0400

    Bump version.

--- src/java/com/stackframe/sarariman/Version.java
+++ src/java/com/stackframe/sarariman/Version.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (C) 2011 StackFrame, LLC
+ * Copyright (C) 2012 StackFrame, LLC
  * This code is licensed under GPLv2.
  */
 package com.stackframe.sarariman;
 
 public class Version {
 
-    public static final String version = "v1.1.35a";
+    public static final String version = "v1.1.36";
 
 }
