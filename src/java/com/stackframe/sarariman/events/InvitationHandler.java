a8ec12a789d71863fae0497598c46db43058a513
Gene McCulley <mcculley@stackframe.com>
2012-11-26 15:58:45 -0500

    Log when the invite is sent out.

--- src/java/com/stackframe/sarariman/events/InvitationHandler.java
+++ src/java/com/stackframe/sarariman/events/InvitationHandler.java
@@ -6,6 +6,10 @@ package com.stackframe.sarariman.events;
 
 import com.stackframe.sarariman.*;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -25,6 +29,23 @@ public class InvitationHandler extends HttpServlet {
         sarariman = (Sarariman)getServletContext().getAttribute("sarariman");
     }
 
+    private void logInvitation(int event, int employee) throws SQLException {
+        Connection connection = sarariman.openConnection();
+        try {
+            PreparedStatement ps = connection.prepareStatement("INSERT INTO company_events_invitation_log (event, employee, invited) VALUES(?, ?, ?)");
+            try {
+                ps.setInt(1, event);
+                ps.setInt(2, employee);
+                ps.setTimestamp(3, new Timestamp(System.currentTimeMillis()));
+                ps.execute();
+            } finally {
+                ps.close();
+            }
+        } finally {
+            connection.close();
+        }
+    }
+
     /**
      * Handles the HTTP
      * <code>POST</code> method.
@@ -37,11 +58,19 @@ public class InvitationHandler extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String eventName = request.getParameter("eventName");
+        int eventID = Integer.parseInt(request.getParameter("event"));
         long employeeID = Long.parseLong(request.getParameter("employee"));
         Employee employee = sarariman.getDirectory().getByNumber().get(employeeID);
         String messageBody = "You are invited to the event '" + eventName + "'.\n\n" + "Go to " + request.getHeader("Referer") + " to RSVP.";
-        String messageSubject = eventName;        
+        String messageSubject = eventName;
         sarariman.getEmailDispatcher().send(employee.getEmail(), null, messageSubject, messageBody);
+        try {
+            logInvitation(eventID, (int)employeeID);
+        } catch (SQLException se) {
+            // FIXME: log this
+            throw new IOException(se);
+        }
+
         response.sendRedirect(request.getHeader("Referer"));
     }
 
