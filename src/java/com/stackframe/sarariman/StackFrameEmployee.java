a728332d7208b76799e2eb25c1033b955f991406
Gene McCulley <mcculley@stackframe.com>
2013-03-13 10:40:41 -0400

    Add administrative assistants to related projects.

--- src/java/com/stackframe/sarariman/StackFrameEmployee.java
+++ src/java/com/stackframe/sarariman/StackFrameEmployee.java
@@ -157,11 +157,32 @@ class StackFrameEmployee extends AbstractLinkable implements Employee {
         try {
             Connection connection = dataSource.getConnection();
             try {
-                PreparedStatement s = connection.prepareStatement("SELECT pm.project " + "FROM project_managers AS pm " + "JOIN projects AS p ON pm.project = p.id " + "WHERE pm.employee = ? AND " + "p.active = TRUE " + "UNION " + "SELECT pm.project " + "FROM project_cost_managers AS pm " + "JOIN projects AS p ON pm.project = p.id " + "WHERE pm.employee = ? AND " + "p.active = TRUE " + "UNION " + "SELECT DISTINCT(p.id) AS project " + "FROM projects AS p " + "JOIN tasks AS t ON t.project = p.id " + "JOIN task_assignments AS ta ON ta.task=t.id " + "WHERE ta.employee = ? AND " + "p.active = TRUE");
+                PreparedStatement s = connection.prepareStatement(
+                        "SELECT pm.project "
+                        + "FROM project_managers AS pm "
+                        + "JOIN projects AS p ON pm.project = p.id "
+                        + "WHERE pm.employee = ? AND "
+                        + "p.active = TRUE "
+                        + "UNION "
+                        + "SELECT pm.project "
+                        + "FROM project_cost_managers AS pm "
+                        + "JOIN projects AS p ON pm.project = p.id "
+                        + "WHERE pm.employee = ? AND "
+                        + "p.active = TRUE "
+                        + "UNION "
+                        + "SELECT DISTINCT(p.id) AS project "
+                        + "FROM projects AS p "
+                        + "JOIN tasks AS t ON t.project = p.id "
+                        + "JOIN task_assignments AS ta ON ta.task=t.id "
+                        + "WHERE ta.employee = ? AND "
+                        + "p.active = TRUE "
+                        + "UNION "
+                        + "SELECT project FROM project_administrative_assistants WHERE assistant = ?");
                 try {
                     s.setInt(1, number);
                     s.setInt(2, number);
                     s.setInt(3, number);
+                    s.setInt(4, number);
                     ResultSet rs = s.executeQuery();
                     try {
                         Collection<Project> c = new ArrayList<Project>();
