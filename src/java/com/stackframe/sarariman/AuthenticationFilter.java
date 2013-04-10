a426080dbcd5ad28bfeef3d14535b8aa974c1444
Gene McCulley <mcculley@stackframe.com>
2013-04-10 17:08:54 -0400

    Remove diagnostics.

--- src/java/com/stackframe/sarariman/AuthenticationFilter.java
+++ src/java/com/stackframe/sarariman/AuthenticationFilter.java
@@ -26,34 +26,27 @@ public class AuthenticationFilter implements Filter {
     }
 
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        System.err.println("entering AuthenticationFilter::doFilter");
         assert request instanceof HttpServletRequest;
         HttpServletRequest httpRequest = (HttpServletRequest)request;
         HttpSession session = httpRequest.getSession();
         Employee user = (Employee)session.getAttribute("user");
         if (user == null) {
-            System.err.println("AuthenticationFilter::doFilter no user");
             String authorizationHeader = httpRequest.getHeader("Authorization");
             HttpServletResponse httpResponse = (HttpServletResponse)response;
             if (authorizationHeader == null) {
                 httpResponse.addHeader("WWW-Authenticate", "Basic realm=\"sarariman\"");
-                System.err.println("AuthenticationFilter::doFilter sending 401");
                 httpResponse.sendError(401);
                 return;
             } else {
-                System.err.printf("Authorization='%s'", authorizationHeader);
                 BASE64Decoder decoder = new BASE64Decoder();
                 String[] split = authorizationHeader.split(" ");
                 String encodedBytes = split[1];
-                System.err.printf("encodedBytes='%s'", encodedBytes);
                 byte[] decodedBytes = decoder.decodeBuffer(encodedBytes);
                 String decodedString = new String(decodedBytes);
-                System.err.printf("decodedString='%s'", decodedString);
                 String[] credentials = decodedString.split(":");
                 int firstColon = decodedString.indexOf(':');
                 String username = decodedString.substring(0, firstColon);
                 String password = decodedString.substring(firstColon + 1);
-                System.err.printf("username='%s'", username);
                 int domainIndex = username.indexOf('@');
                 if (domainIndex == -1) {
                     httpResponse.sendError(401, "username must be in the form of an email address (user@example.com)");
@@ -61,14 +54,10 @@ public class AuthenticationFilter implements Filter {
                 }
 
                 username = username.substring(0, domainIndex); // FIXME: Check for proper domain and dispatch.
-                System.err.printf("username='%s'", username);
-                System.err.printf("password='%s'", password);
                 Sarariman sarariman = (Sarariman)request.getServletContext().getAttribute("sarariman");
                 Directory directory = sarariman.getDirectory();
                 boolean valid = directory.checkCredentials(username, password);
-                System.err.printf("valid='%b'", valid);
                 if (valid) {
-                    System.err.println("AuthenticationFilter::doFilter valid!");
                     user = directory.getByUserName().get(username);
                     session.setAttribute("user", user);
                 } else {
@@ -76,13 +65,10 @@ public class AuthenticationFilter implements Filter {
                     return;
                 }
             }
-        } else {
-            System.err.printf("user=%s\n", user.getUserName());
         }
 
         request.setAttribute("user", user);
         chain.doFilter(request, response);
-        System.err.println("leaving AuthenticationFilter::doFilter");
     }
 
     public void destroy() {
