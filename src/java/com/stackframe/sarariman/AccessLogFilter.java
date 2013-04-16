cf5c7c9aa5fa98166b61b3d27ae71083b9ab5b5c
Gene McCulley <mcculley@stackframe.com>
2013-04-16 12:43:12 -0400

    Do the log insert in a background thread to improve interactive performance.

--- src/java/com/stackframe/sarariman/AccessLogFilter.java
+++ src/java/com/stackframe/sarariman/AccessLogFilter.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -25,6 +27,7 @@ import javax.sql.DataSource;
 public class AccessLogFilter implements Filter {
 
     private DataSource dataSource;
+    private final ExecutorService executor = Executors.newFixedThreadPool(1);
 
     public void init(FilterConfig filterConfig) throws ServletException {
         Sarariman sarariman = (Sarariman)filterConfig.getServletContext().getAttribute("sarariman");
@@ -33,54 +36,70 @@ public class AccessLogFilter implements Filter {
 
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         // FIXME: This is a hack because the deployed version of Tomcat doesn't support getStatus(). Ditch it when we upgrade.
-        StatusExposingServletResponse sesr = new StatusExposingServletResponse((HttpServletResponse)response);
+        final StatusExposingServletResponse sesr = new StatusExposingServletResponse((HttpServletResponse)response);
 
         long start = System.currentTimeMillis();
         chain.doFilter(request, sesr);
         long stop = System.currentTimeMillis();
-        long took = stop - start;
+        final long took = stop - start;
+        
+        // One would think that the httpServletRequest could just be marked final and used in the Runnable, but it gets reused after
+        // this call.
         HttpServletRequest httpServletRequest = (HttpServletRequest)request;
-        Employee employee = (Employee)request.getAttribute("user");
-        try {
-            Connection c = dataSource.getConnection();
-            try {
-                PreparedStatement s = c.prepareStatement(
-                        "INSERT INTO access_log (path, query, method, employee, status, time, user_agent, remote_address) " +
-                        "VALUES(?, ?, ?, ?, ?, ?, ?, ?)");
+        final String path = httpServletRequest.getServletPath();
+        final String queryString = httpServletRequest.getQueryString();
+        final String method = httpServletRequest.getMethod();
+        final String userAgent = httpServletRequest.getHeader("User-Agent");
+        final String remoteAddress = httpServletRequest.getRemoteAddr();
+        
+        final Employee employee = (Employee)request.getAttribute("user");
+        Runnable insertTask = new Runnable() {
+            public void run() {
                 try {
-                    s.setString(1, httpServletRequest.getServletPath());
-                    s.setString(2, httpServletRequest.getQueryString());
-                    s.setString(3, httpServletRequest.getMethod());
-                    if (employee == null) {
-                        s.setObject(4, null);
-                    } else {
-                        s.setInt(4, employee.getNumber());
-                    }
+                    Connection c = dataSource.getConnection();
+                    try {
+                        PreparedStatement s = c.prepareStatement(
+                                "INSERT INTO access_log (path, query, method, employee, status, time, user_agent, remote_address) " +
+                                "VALUES(?, ?, ?, ?, ?, ?, ?, ?)");
+                        try {
+                            s.setString(1, path);
+                            s.setString(2, queryString);
+                            s.setString(3, method);
+                            if (employee == null) {
+                                s.setObject(4, null);
+                            } else {
+                                s.setInt(4, employee.getNumber());
+                            }
 
-                    // We will always get 0 if no error is set, because of the aforementioned Tomcat/Servlet spec. issue.
-                    int status = sesr.getStatus();
-                    if (status == 0) {
-                        status = 200;
-                    }
+                            // We will always get 0 if no error is set, because of the aforementioned Tomcat/Servlet spec. issue.
+                            int status = sesr.getStatus();
+                            if (status == 0) {
+                                status = 200;
+                            }
 
-                    s.setInt(5, status);
-                    s.setLong(6, took);
-                    s.setString(7, httpServletRequest.getHeader("User-Agent"));
-                    s.setString(8, httpServletRequest.getRemoteAddr());
-                    int numRowsInserted = s.executeUpdate();
-                    assert numRowsInserted == 1;
-                } finally {
-                    s.close();
+                            s.setInt(5, status);
+                            s.setLong(6, took);
+                            s.setString(7, userAgent);
+                            s.setString(8, remoteAddress);
+                            int numRowsInserted = s.executeUpdate();
+                            assert numRowsInserted == 1;
+                        } finally {
+                            s.close();
+                        }
+                    } finally {
+                        c.close();
+                    }
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
                 }
-            } finally {
-                c.close();
             }
-        } catch (SQLException e) {
-            throw new ServletException(e);
-        }
+
+        };
+        executor.execute(insertTask);
     }
 
     public void destroy() {
+        executor.shutdown();
     }
 
 }
