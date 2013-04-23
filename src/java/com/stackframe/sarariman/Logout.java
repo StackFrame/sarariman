05d6a3b7ddbf0795444756c15180b86afe9cfa95
Gene McCulley <mcculley@stackframe.com>
2013-04-23 12:30:49 -0400

    Support form login.

---
+++ src/java/com/stackframe/sarariman/Logout.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2013 StackFrame, LLC
+ * This code is licensed under GPLv2.
+ */
+package com.stackframe.sarariman;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author mcculley
+ */
+public class Logout extends HttpServlet {
+
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        session.setAttribute("user", null);
+        response.sendRedirect(response.encodeRedirectURL(request.getContextPath() + "/login"));
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Handler for logging out of a session";
+    }
+
+}
