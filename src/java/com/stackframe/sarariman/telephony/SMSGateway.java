ad8489cdfd2b1b0bbab4291bec83abc5937fec81
Gene McCulley <mcculley@stackframe.com>
2013-05-20 20:48:16 -0400

    Add interface for sending SMS.

---
+++ src/java/com/stackframe/sarariman/telephony/SMSGateway.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2013 StackFrame, LLC
+ * This code is licensed under GPLv2.
+ */
+package com.stackframe.sarariman.telephony;
+
+/**
+ *
+ * @author mcculley
+ */
+public interface SMSGateway {
+
+    void send(String to, String body) throws Exception;
+
+}
