bf31c6b98a18d1bc95f05e072e629c776ea8c664
Gene McCulley <mcculley@stackframe.com>
2013-05-30 07:02:21 -0400

    Add some diagnostics.

--- src/java/com/stackframe/sarariman/xmpp/vysper/ArchivedDiscussionHistory.java
+++ src/java/com/stackframe/sarariman/xmpp/vysper/ArchivedDiscussionHistory.java
@@ -5,8 +5,11 @@
 package com.stackframe.sarariman.xmpp.vysper;
 
 import java.util.Calendar;
+import org.apache.vysper.xml.fragment.XMLSemanticError;
+import org.apache.vysper.xmpp.addressing.Entity;
 import org.apache.vysper.xmpp.modules.extension.xep0045_muc.model.DiscussionHistory;
 import org.apache.vysper.xmpp.modules.extension.xep0045_muc.model.Occupant;
+import org.apache.vysper.xmpp.stanza.MessageStanza;
 import org.apache.vysper.xmpp.stanza.Stanza;
 
 /**
@@ -21,6 +24,16 @@ public class ArchivedDiscussionHistory extends DiscussionHistory {
     public void append(Stanza stanza, Occupant sender, Calendar timestamp) {
         // FIXME archive to database
         System.err.println("stanza that would be archived sent to room . stanza=" + stanza);
+        Entity from = stanza.getFrom();
+        if (MessageStanza.isOfType(stanza)) {
+            MessageStanza message = new MessageStanza(stanza);
+            try {
+                String body = message.getBody(null);
+                System.err.println("from=" + from + " body='" + body + "'");
+            } catch (XMLSemanticError e) {
+                e.printStackTrace();
+            }
+        }
         super.append(stanza, sender, timestamp);
     }
 
