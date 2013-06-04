afaf1770f74845fbe4185ddb7972147b73212750
Gene McCulley <mcculley@stackframe.com>
2013-06-04 07:51:14 -0400

    Add DeploymentMode type.

---
+++ src/java/com/stackframe/sarariman/DeploymentMode.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2013 StackFrame, LLC
+ * This code is licensed under GPLv2.
+ */
+package com.stackframe.sarariman;
+
+/**
+ *
+ * @author mcculley
+ */
+public enum DeploymentMode {
+
+    development, production
+
+}
