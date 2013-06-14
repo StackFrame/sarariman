0528472cdc7c80d873d294b541fd12255a4ede18
Gene McCulley <mcculley@stackframe.com>
2013-06-14 15:56:44 -0400

    Handle POST.

--- src/java/com/stackframe/sarariman/RootServlet.java
+++ src/java/com/stackframe/sarariman/RootServlet.java
@@ -229,6 +229,11 @@ public class RootServlet extends WebDAVServlet {
         request.getRequestDispatcher("home.jsp").forward(request, response);
     }
 
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("home.jsp").forward(request, response);
+    }
+
     /**
      * Returns a short description of the servlet.
      *
