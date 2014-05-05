e60f4eb7cbe7b532bd538f6dc143ebaa103f4f83
Gene McCulley <mcculley@stackframe.com>
2014-05-05 12:30:16 -0400

    Use more idiomatic Java 8 style. Add Override annotation.

--- src/java/com/stackframe/sarariman/AccessLogFilter.java
+++ src/java/com/stackframe/sarariman/AccessLogFilter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 StackFrame, LLC
+ * Copyright (C) 2013-2014 StackFrame, LLC
  * This code is licensed under GPLv2.
  */
 package com.stackframe.sarariman;
@@ -26,12 +26,14 @@ public class AccessLogFilter extends HttpFilter {
 
     private Executor executor;
 
+    @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         Sarariman sarariman = (Sarariman)filterConfig.getServletContext().getAttribute("sarariman");
         dataSource = sarariman.getDataSource();
         executor = sarariman.getBackgroundDatabaseWriteExecutor();
     }
 
+    @Override
     protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
         // FIXME: This is a hack because the deployed version of Tomcat doesn't support getStatus(). Ditch it when we upgrade.
         final StatusExposingServletResponse sesr = new StatusExposingServletResponse(response);
@@ -52,54 +54,43 @@ public class AccessLogFilter extends HttpFilter {
         final String host = request.getHeader("Host");
 
         final Employee employee = (Employee)request.getAttribute("user");
-        Runnable insertTask = new Runnable() {
-            public void run() {
-                try {
-                    Connection c = dataSource.getConnection();
-                    try {
-                        PreparedStatement s = c.prepareStatement(
-                                "INSERT INTO access_log (path, query, method, employee, status, time, user_agent, remote_address, referrer, host) " +
-                                "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-                        try {
-                            s.setString(1, path);
-                            s.setString(2, queryString);
-                            s.setString(3, method);
-                            if (employee == null) {
-                                s.setObject(4, null);
-                            } else {
-                                s.setInt(4, employee.getNumber());
-                            }
-
-                            // We will always get 0 if no error is set, because of the aforementioned Tomcat/Servlet spec. issue.
-                            int status = sesr.getStatus();
-                            if (status == 0) {
-                                status = 200;
-                            }
-
-                            s.setInt(5, status);
-                            s.setLong(6, took);
-                            s.setString(7, userAgent);
-                            s.setString(8, remoteAddress);
-                            s.setString(9, referrer);
-                            s.setString(10, host);
-                            int numRowsInserted = s.executeUpdate();
-                            assert numRowsInserted == 1;
-                        } finally {
-                            s.close();
-                        }
-                    } finally {
-                        c.close();
-                    }
-                } catch (SQLException e) {
-                    // FIXME: Should we log this exception? Does it kill the Executor?
-                    throw new RuntimeException(e);
+        Runnable insertTask = () -> {
+            try (Connection c = dataSource.getConnection();
+                 PreparedStatement s = c.prepareStatement(
+                         "INSERT INTO access_log (path, query, method, employee, status, time, user_agent, remote_address, referrer, host) " +
+                         "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");) {
+                s.setString(1, path);
+                s.setString(2, queryString);
+                s.setString(3, method);
+                if (employee == null) {
+                    s.setObject(4, null);
+                } else {
+                    s.setInt(4, employee.getNumber());
                 }
-            }
 
+                // We will always get 0 if no error is set, because of the aforementioned Tomcat/Servlet spec. issue.
+                int status = sesr.getStatus();
+                if (status == 0) {
+                    status = 200;
+                }
+
+                s.setInt(5, status);
+                s.setLong(6, took);
+                s.setString(7, userAgent);
+                s.setString(8, remoteAddress);
+                s.setString(9, referrer);
+                s.setString(10, host);
+                int numRowsInserted = s.executeUpdate();
+                assert numRowsInserted == 1;
+            } catch (SQLException e) {
+                // FIXME: Should we log this exception? Does it kill the Executor?
+                throw new RuntimeException(e);
+            }
         };
         executor.execute(insertTask);
     }
 
+    @Override
     public void destroy() {
     }
 
