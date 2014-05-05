91937bf1ac3f25014485179df6eddfdcc8d9bd09
Gene McCulley <mcculley@stackframe.com>
2014-05-05 13:07:14 -0400

    Use more idiomatic Java 8 style.

--- src/java/com/stackframe/sarariman/WorkdaysImpl.java
+++ src/java/com/stackframe/sarariman/WorkdaysImpl.java
@@ -1,17 +1,17 @@
 /*
- * Copyright (C) 2013 StackFrame, LLC
+ * Copyright (C) 2013-2014 StackFrame, LLC
  * This code is licensed under GPLv2.
  */
 package com.stackframe.sarariman;
 
-import com.google.common.base.Predicate;
-import com.google.common.collect.Collections2;
 import com.stackframe.sarariman.holidays.Holidays;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -25,21 +25,19 @@ public class WorkdaysImpl implements Workdays {
         this.holidays = holidays;
     }
 
-    private static final Predicate<Date> isWeekDay = new Predicate<Date>() {
-        public boolean apply(Date t) {
-            Calendar calendar = new GregorianCalendar();
-            calendar.setTime(t);
-            int day = calendar.get(Calendar.DAY_OF_WEEK);
-            return day != Calendar.SATURDAY && day != Calendar.SUNDAY;
-        }
-
+    private static final Predicate<Date> isWeekDay = (Date t) -> {
+        Calendar calendar = new GregorianCalendar();
+        calendar.setTime(t);
+        int day = calendar.get(Calendar.DAY_OF_WEEK);
+        return day != Calendar.SATURDAY && day != Calendar.SUNDAY;
     };
 
+    @Override
     public Collection<Date> getWorkdays(PeriodOfPerformance pop) {
         Collection<Date> holidaysToExclude = holidays.get(pop.asRange());
         Collection<Date> allDays = pop.getDays();
-        Collection<Date> weekDays = Collections2.filter(allDays, isWeekDay);
-        Collection<Date> workingDays = new ArrayList(weekDays);
+        Collection<Date> weekDays = allDays.stream().filter(isWeekDay).collect(Collectors.toList());
+        Collection<Date> workingDays = new ArrayList<>(weekDays);
         workingDays.removeAll(holidaysToExclude);
         return workingDays;
     }
