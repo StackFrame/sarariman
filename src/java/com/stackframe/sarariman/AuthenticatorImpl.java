16c0ca29892214816b1f0e5140ceec6ecf594d9b
Gene McCulley <mcculley@stackframe.com>
2014-05-07 10:58:51 -0400

    Various cleanups.

--- src/java/com/stackframe/sarariman/AuthenticatorImpl.java
+++ src/java/com/stackframe/sarariman/AuthenticatorImpl.java
@@ -1,8 +1,10 @@
 /*
- * Copyright (C) 2013 StackFrame, LLC
+ * Copyright (C) 2013-2014 StackFrame, LLC
  * This code is licensed under GPLv2.
  */
 package com.stackframe.sarariman;
+
+import java.util.logging.Logger;
 
 /**
  *
@@ -12,17 +14,20 @@ public class AuthenticatorImpl implements Authenticator {
 
     private final Authenticator stackFrameAuthenticator;
 
+    private final Logger logger = Logger.getLogger(getClass().getName());
+
     public AuthenticatorImpl(Authenticator stackFrameAuthenticator) {
         this.stackFrameAuthenticator = stackFrameAuthenticator;
     }
 
+    @Override
     public boolean checkCredentials(String username, String password) {
         int domainIndex = username.indexOf('@');
         if (domainIndex != -1) {
             String domain = username.substring(domainIndex + 1);
             if (!"stackframe.com".equals(domain)) {
                 // FIXME: Here is where we would dispatch to validate external users.
-                System.err.println("Attempted log in of non-StackFrame resource. domain=" + domain);
+                logger.warning("Attempted log in of non-StackFrame resource. domain=" + domain);
                 return false;
             }
 
