66dc8eca596bc4888c862ce659d7fc2a4e80eb3d
Gene McCulley <mcculley@stackframe.com>
2014-05-07 07:52:04 -0400

    Various cleanups for Java 8.

--- src/java/com/stackframe/sarariman/Invoice.java
+++ src/java/com/stackframe/sarariman/Invoice.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009-2013 StackFrame, LLC
+ * Copyright (C) 2009-2014 StackFrame, LLC
  * This code is licensed under GPLv2.
  */
 package com.stackframe.sarariman;
@@ -32,6 +32,7 @@ import javax.sql.DataSource;
 public class Invoice {
 
     private final String id;
+
     private final Sarariman sarariman;
 
     public Invoice(String id, Sarariman sarariman) {
@@ -40,7 +41,9 @@ public class Invoice {
     }
 
 // Add a safety check to make sure that no elements are invoiced outside of the PoP
-    public static Invoice create(Sarariman sarariman, Client client, Project project, String popStart, String popEnd, Map parameterMap, String[] employees, String[] tasks, String[] dates, String[] billedServices) throws SQLException, ParseException {
+    public static Invoice create(Sarariman sarariman, Client client, Project project, String popStart, String popEnd,
+                                 Map<String, String[]> parameterMap, String[] employees, String[] tasks, String[] dates,
+                                 String[] billedServices) throws SQLException, ParseException {
         Preconditions.checkNotNull(client);
         if (employees == null) {
             employees = new String[0];
@@ -60,24 +63,26 @@ public class Invoice {
         }
 
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        Connection connection = sarariman.openConnection();
-        try {
+        try (Connection connection = sarariman.openConnection()) {
             connection.setAutoCommit(false);
 
-            PreparedStatement createInvoice = connection.prepareStatement("INSERT INTO invoice_info (sent, customer, project, pop_start, pop_end, description) VALUES(?, ?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
-            createInvoice.setDate(1, convert(new java.util.Date()));
-            createInvoice.setLong(2, client.getId());
-            createInvoice.setLong(3, project.getId());
-            createInvoice.setDate(4, convert(dateFormat.parse(popStart)));
-            createInvoice.setDate(5, convert(dateFormat.parse(popEnd)));
-            createInvoice.setString(6, "invoice for " + client.getName() + " - " + project.getName());
-            int createdRowCount = createInvoice.executeUpdate();
-            assert createdRowCount == 1;
-            ResultSet keys = createInvoice.getGeneratedKeys();
-            keys.next();
-            int key = keys.getInt(1);
-            String id = Integer.toString(key);
-            createInvoice.close();
+            String id;
+            try (PreparedStatement createInvoice = connection.prepareStatement(
+                    "INSERT INTO invoice_info (sent, customer, project, pop_start, pop_end, description) VALUES(?, ?, ?, ?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS)) {
+                createInvoice.setDate(1, convert(new java.util.Date()));
+                createInvoice.setLong(2, client.getId());
+                createInvoice.setLong(3, project.getId());
+                createInvoice.setDate(4, convert(dateFormat.parse(popStart)));
+                createInvoice.setDate(5, convert(dateFormat.parse(popEnd)));
+                createInvoice.setString(6, "invoice for " + client.getName() + " - " + project.getName());
+                int createdRowCount = createInvoice.executeUpdate();
+                assert createdRowCount == 1;
+                ResultSet keys = createInvoice.getGeneratedKeys();
+                keys.next();
+                int key = keys.getInt(1);
+                id = Integer.toString(key);
+            }
 
             for (int i = 0; i < numItems; i++) {
                 String toAdd = "addToInvoice" + i;
@@ -85,8 +90,8 @@ public class Invoice {
                     int employeeNumber = Integer.parseInt(employees[i]);
                     int task = Integer.parseInt(tasks[i]);
                     Date date = Date.valueOf(dates[i]);
-                    PreparedStatement ps = connection.prepareStatement("INSERT INTO invoices (id, employee, task, date) VALUES(?, ?, ?, ?)");
-                    try {
+                    try (PreparedStatement ps = connection.prepareStatement(
+                            "INSERT INTO invoices (id, employee, task, date) VALUES(?, ?, ?, ?)")) {
                         ps.setString(1, id);
                         ps.setInt(2, employeeNumber);
                         ps.setInt(3, task);
@@ -95,24 +100,19 @@ public class Invoice {
                         if (rowCount != 1) {
                             System.err.println("could not insert invoice row for date=" + date + " and employee=" + employeeNumber);
                         }
-                    } finally {
-                        ps.close();
                     }
                 }
             }
 
             if (billedServices != null) {
                 for (String billedService : billedServices) {
-                    PreparedStatement ps = connection.prepareStatement("UPDATE billed_services SET invoice=? WHERE id=?");
-                    try {
+                    try (PreparedStatement ps = connection.prepareStatement("UPDATE billed_services SET invoice=? WHERE id=?")) {
                         ps.setString(1, id);
                         ps.setString(2, billedService);
                         int rowCount = ps.executeUpdate();
                         if (rowCount != 1) {
                             System.err.println("could not invoice billed service for id=" + billedService);
                         }
-                    } finally {
-                        ps.close();
                     }
                 }
             }
@@ -123,43 +123,32 @@ public class Invoice {
             sarariman.getEmailDispatcher().send(EmailDispatcher.addresses(sarariman.getInvoiceManagers()), null, "invoice created",
                                                 "Invoice " + id + " was created.");
             return new Invoice(id, sarariman);
-        } finally {
-            connection.close();
         }
     }
 
     public void delete() throws SQLException {
-        Connection connection = sarariman.openConnection();
-        try {
+        try (Connection connection = sarariman.openConnection()) {
             connection.setAutoCommit(false);
-            PreparedStatement deleteHours = connection.prepareStatement("DELETE FROM invoices WHERE id = ?");
-            try {
+            try (PreparedStatement deleteHours = connection.prepareStatement("DELETE FROM invoices WHERE id = ?")) {
                 deleteHours.setString(1, id);
                 int rowCount = deleteHours.executeUpdate();
                 if (rowCount == 0) {
                     System.err.println("could not delete invoice " + id);
                 } else {
-                    PreparedStatement deleteInfo = connection.prepareStatement("DELETE FROM invoice_info WHERE id = ?");
-                    try {
+                    try (PreparedStatement deleteInfo = connection.prepareStatement("DELETE FROM invoice_info WHERE id = ?")) {
                         deleteInfo.setString(1, id);
                         rowCount = deleteInfo.executeUpdate();
                         if (rowCount != 1) {
                             System.err.println("could not delete invoice " + id);
                         } else {
-                            PreparedStatement deleteExpenses = connection.prepareStatement("UPDATE expenses SET invoice = NULL WHERE invoice = ?");
-                            try {
+                            try (PreparedStatement deleteExpenses = connection.prepareStatement(
+                                    "UPDATE expenses SET invoice = NULL WHERE invoice = ?")) {
                                 deleteExpenses.setString(1, id);
                                 deleteExpenses.executeUpdate();
-                            } finally {
-                                deleteExpenses.close();
                             }
                         }
-                    } finally {
-                        deleteInfo.close();
                     }
                 }
-            } finally {
-                deleteHours.close();
             }
 
             connection.commit();
@@ -167,69 +156,65 @@ public class Invoice {
 
             sarariman.getEmailDispatcher().send(EmailDispatcher.addresses(sarariman.getInvoiceManagers()), null, "invoice deleted",
                                                 "Invoice " + id + " was deleted.");
-        } finally {
-            connection.close();
         }
     }
 
-    public static CostData cost(Sarariman sarariman, int project, int employee, int task, Date date, double duration) throws SQLException {
+    public static CostData cost(Sarariman sarariman, int project, int employee, int task, Date date, double duration)
+            throws SQLException {
         Map<Long, LaborCategory> categoriesById = sarariman.getLaborCategories();
         Collection<LaborCategoryAssignment> projectBillRates = sarariman.getProjectBillRates();
         return cost(sarariman, categoriesById, projectBillRates, project, employee, task, date, duration);
     }
 
-    public static CostData cost(Sarariman sarariman, Map<Long, LaborCategory> categoriesById, Collection<LaborCategoryAssignment> projectBillRates, int project, int employee, int task_id, Date date, double duration) throws SQLException {
-        return cost(categoriesById, projectBillRates, project, employee, task_id, date, duration, sarariman.getTasks(), sarariman.getDataSource());
+    public static CostData cost(Sarariman sarariman, Map<Long, LaborCategory> categoriesById,
+                                Collection<LaborCategoryAssignment> projectBillRates, int project, int employee, int task_id,
+                                Date date, double duration) throws SQLException {
+        return cost(categoriesById, projectBillRates, project, employee, task_id, date, duration, sarariman.getTasks(),
+                    sarariman.getDataSource());
     }
 
-    public static CostData cost(Map<Long, LaborCategory> categoriesById, Collection<LaborCategoryAssignment> projectBillRates, int project, int employee, int task_id, Date date, double duration, Tasks tasks, DataSource dataSource) throws SQLException {
+    public static CostData cost(Map<Long, LaborCategory> categoriesById, Collection<LaborCategoryAssignment> projectBillRates,
+                                int project, int employee, int task_id, Date date, double duration, Tasks tasks,
+                                DataSource dataSource) throws SQLException {
         return cost(categoriesById, projectBillRates, project, employee, tasks.get(task_id), date, duration, dataSource);
     }
 
     public static CostData cost(Map<Long, LaborCategory> categoriesById, Collection<LaborCategoryAssignment> projectBillRates,
                                 int project, int employee, Task task, Date date, double duration, DataSource dataSource)
             throws SQLException {
-        Connection c = dataSource.getConnection();
-        try {
-            PreparedStatement s = c.prepareStatement(
-                    "SELECT labor_categories.rate, labor_categories.id " +
-                    "FROM labor_categories " +
-                    "JOIN labor_category_assignments ON labor_categories.id = labor_category_assignments.labor_category " +
-                    "WHERE labor_categories.project = ? AND " +
-                    "labor_category_assignments.employee = ? AND " +
-                    "labor_categories.pop_start <= ? AND " +
-                    "labor_categories.pop_end >= ? AND " +
-                    "labor_category_assignments.pop_start <= ? AND " +
-                    "labor_category_assignments.pop_end >= ?");
-            try {
-                s.setInt(1, project);
-                s.setInt(2, employee);
-                s.setDate(3, date);
-                s.setDate(4, date);
-                s.setDate(5, date);
-                s.setDate(6, date);
-                ResultSet r = s.executeQuery();
-                if (r.first()) {
-                    BigDecimal rate;
-                    BigDecimal cost;
-                    Long categoryId = r.getLong("id");
-                    if (task.isBillable()) {
-                        rate = r.getBigDecimal("rate").setScale(2);
-                        cost = rate.multiply(new BigDecimal(duration));
-                        cost = cost.setScale(2, RoundingMode.UP);
-                    } else {
-                        rate = cost = BigDecimal.ZERO;
-                    }
-
-                    return new CostData(cost, categoriesById.get(categoryId), rate);
+        try (Connection c = dataSource.getConnection(); PreparedStatement s = c.prepareStatement(
+             "SELECT labor_categories.rate, labor_categories.id " +
+             "FROM labor_categories " +
+             "JOIN labor_category_assignments ON labor_categories.id = labor_category_assignments.labor_category " +
+             "WHERE labor_categories.project = ? AND " +
+             "labor_category_assignments.employee = ? AND " +
+             "labor_categories.pop_start <= ? AND " +
+             "labor_categories.pop_end >= ? AND " +
+             "labor_category_assignments.pop_start <= ? AND " +
+             "labor_category_assignments.pop_end >= ?")) {
+            s.setInt(1, project);
+            s.setInt(2, employee);
+            s.setDate(3, date);
+            s.setDate(4, date);
+            s.setDate(5, date);
+            s.setDate(6, date);
+            ResultSet r = s.executeQuery();
+            if (r.first()) {
+                BigDecimal rate;
+                BigDecimal cost;
+                Long categoryId = r.getLong("id");
+                if (task.isBillable()) {
+                    rate = r.getBigDecimal("rate").setScale(2);
+                    cost = rate.multiply(new BigDecimal(duration));
+                    cost = cost.setScale(2, RoundingMode.UP);
                 } else {
-                    return new CostData(new BigDecimal(0), null, new BigDecimal(0));
+                    rate = cost = BigDecimal.ZERO;
                 }
-            } finally {
-                s.close();
+
+                return new CostData(cost, categoriesById.get(categoryId), rate);
+            } else {
+                return new CostData(new BigDecimal(0), null, new BigDecimal(0));
             }
-        } finally {
-            c.close();
         }
     }
 
